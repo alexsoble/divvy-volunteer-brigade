622129a219b7e01f0ac4d4b83fc26f78e56d25df
Alex Soble <asoble@gmail.com>
2016-09-04 18:03:03 -0400

    Pull JS and CSS into files

---
+++ map.js
@@ -0,0 +1,33 @@
+$(function() {
+  var map = L.map('map').setView([41.89, -87.61], 12);
+
+  var layer = new L.StamenTileLayer("toner-lite");
+  map.addLayer(layer);
+
+  var bikeIcon = L.divIcon({
+    className: 'icon-wrapper',
+    html: '<span class="map-icon map-icon-bicycling"><span>'
+  });
+
+  $.ajax({
+    url: 'http://www.divvybikes.com/stations/json/',
+    success: function (data) {
+      var stationList = data.stationBeanList;
+      $.each(stationList, function(i, station) {
+        var availableBikes = station.availableBikes;
+        var availableDocks = station.availableDocks;
+        var stationName = station.stationName;
+        var latitude = station.latitude;
+        var longitude = station.longitude;
+
+        if (availableBikes === 0) {
+          L.marker([latitude, longitude], { icon: bikeIcon }).addTo(map)
+            .bindPopup('Zero bikes at ' + stationName + '!');
+        } else if (availableDocks === 0) {
+          L.marker([latitude, longitude], { icon: bikeIcon }).addTo(map)
+            .bindPopup('Zero bikes at ' + stationName + '!');
+        };
+      });
+    }
+  });
+});
