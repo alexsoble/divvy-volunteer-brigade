b7b6a4b2c5bb7aa3150360b12227aa7a2b52a445
Alex Soble <asoble@gmail.com>
2016-09-05 08:42:17 -0400

    Pull out the main function

--- map.js
+++ map.js
@@ -9,25 +9,27 @@ $(function() {
     html: '<span class="map-icon map-icon-bicycling"><span>'
   });
 
+  function handleStationData (data) {
+    var stationList = data.stationBeanList;
+    $.each(stationList, function(i, station) {
+      var availableBikes = station.availableBikes;
+      var availableDocks = station.availableDocks;
+      var stationName = station.stationName;
+      var latitude = station.latitude;
+      var longitude = station.longitude;
+
+      if (availableBikes === 0) {
+        L.marker([latitude, longitude], { icon: bikeIcon }).addTo(map)
+          .bindPopup('Zero bikes at ' + stationName + '!');
+      } else if (availableDocks === 0) {
+        L.marker([latitude, longitude], { icon: bikeIcon }).addTo(map)
+          .bindPopup('Zero bikes at ' + stationName + '!');
+      };
+    });
+  }
+
   $.ajax({
     url: 'http://www.divvybikes.com/stations/json/',
-    success: function (data) {
-      var stationList = data.stationBeanList;
-      $.each(stationList, function(i, station) {
-        var availableBikes = station.availableBikes;
-        var availableDocks = station.availableDocks;
-        var stationName = station.stationName;
-        var latitude = station.latitude;
-        var longitude = station.longitude;
-
-        if (availableBikes === 0) {
-          L.marker([latitude, longitude], { icon: bikeIcon }).addTo(map)
-            .bindPopup('Zero bikes at ' + stationName + '!');
-        } else if (availableDocks === 0) {
-          L.marker([latitude, longitude], { icon: bikeIcon }).addTo(map)
-            .bindPopup('Zero bikes at ' + stationName + '!');
-        };
-      });
-    }
+    success: handleStationData
   });
 });
