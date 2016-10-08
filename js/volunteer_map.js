0aa382f8aa344c4d9d3c752e066684c228b2ff17
Alex Soble <asoble@gmail.com>
2016-10-07 19:29:22 -0500

    Add lines to map

--- js/volunteer_map.js
+++ js/volunteer_map.js
@@ -3,14 +3,14 @@
   var VolunteerMap = function initializeVolunteerMap (data, map) {
     this.stationList = data.stationBeanList;
     this.map = map;
-  }
+  };
 
   VolunteerMap.prototype.draw = function draw () {
     var self = this;
     $.each(this.stationList, function(i, station) {
       self.makeMarkerForStation(station);
     });
-  }
+  };
 
   VolunteerMap.prototype.makeMarkerForStation = function makeMarker (station) {
     var availableBikes = station.availableBikes;
@@ -18,6 +18,8 @@
     var stationName = station.stationName;
     var latitude = station.latitude;
     var longitude = station.longitude;
+    var self = this;
+
     var bikeIcon = L.divIcon({
       className: 'icon-wrapper',
       html: '<span class="map-icon map-icon-bicycle-store"><span>'
@@ -29,9 +31,34 @@
 
     if (availableBikes === 0) {
       marker.bindPopup('Zero bikes at ' + stationName + '!');
+      self.drawLinesForStation(station);
     } else if (availableDocks === 0) {
       marker.bindPopup('Zero docks at ' + stationName + '!');
+      self.drawLinesForStation(station);
     };
+  };
+
+  VolunteerMap.prototype.drawLinesForStation = function drawLines (station) {
+    var self = this;
+    var nearest = nearestStationsLookup[station.id];
+
+    var stationCoordinates = new L.LatLng(station.latitude, station.longitude);
+
+    for (i = 0; i < nearest.length; i++) {
+      var nearbyStation = nearest[i];
+      var nearbyCoordinates = new L.LatLng(nearbyStation.latitude, nearbyStation.longitude);
+
+      var pointList = [stationCoordinates, nearbyCoordinates];
+
+      var line = new L.polyline(pointList, {
+        color: 'red',
+        weight: 6,
+        opacity: 0.75,
+        smoothFactor: 1,
+      });
+
+      line.addTo(this.map);
+    }
   };
 
   root.VolunteerMap = VolunteerMap;
